071dfd01e341ee8a8e515600a2895c0e182d1c8e
crazyguy-67 <crazy2hell67@gmail.com>
2026-05-16 14:18:45 +0530

    basic function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
-// for loop
-for (let i = 1; i <= 5; i++) {
-  console.log(i);
+function greet() {
+  console.log("Hello Abhishek");
 }
 
-// while loop
-let count = 1;
-
-while (count <= 5) {
-  console.log(count);
-  count++;
-}
-
-// for ..of
-const fruits = ["apple", "banana", "gauva"];
-
-for (const fruit of fruits) {
-  console.log(fruit);
-}
-
-// Multiplication table example:
-
-const number = 5;
-
-for (let i = 1; i < number; i++) {
-  console.log(`${number}*${i} = ${number * i}`);
-}
+greet();
