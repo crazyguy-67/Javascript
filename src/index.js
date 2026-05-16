eef9d3ed4458ddcc054ea4c99a1c0581babc8dcb
Abhishek <abhishekwx7@gmail.com>
2026-05-16 13:42:33 +0530

    basic javascript

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let name = "Abhishek";
+const age = 22;
+let isStudent = true;
+let salary = null;
+let city;
+
+// let = value can change
+// const = value cannot be reassigned
+// var = old way, avoid for now
+
+const name = "Abhishek";
+let age = 25;
+let city = "Ranchi";
+
+console.log(name);
+console.log(age);
+console.log(city);
