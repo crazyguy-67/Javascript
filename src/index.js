21c50673ed1b21af79c80c3173b207ea0799a948
crazyguy-67 <crazy2hell67@gmail.com>
2026-05-16 16:16:07 +0530

    notes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
-// promise chaining
-
-function getUsers() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ id: 1, name: "Abhishek" });
-    }, 2000);
-  });
-}
-
-function getPosts(userId) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve([
-        { id: 101, title: "Post 1", userId },
-        { id: 102, title: "Post 2", userId },
-      ]);
-    }, 2000);
-  });
-}
-
-getUsers()
-  .then((user) => {
-    console.log("User : ", user);
-    return getPosts(user.id);
-  })
-  .then((posts) => {
-    console.log("Posts : ", posts);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// callback    = function passed to run later
+// promise     = future success/failure value
+// .then       = handles success
+// .catch      = handles error
+// async       = makes function async
+// await       = waits for promise
+// try/catch   = handles async/await errors
