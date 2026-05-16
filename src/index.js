265ea760d3d4ac43e976e0500293d8a7d3b582a4
crazyguy-67 <crazy2hell67@gmail.com>
2026-05-16 16:11:35 +0530

    promise chaining

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-// promise failure example
+// promise chaining
 
-function loginUser(email, password) {
-  return new Promise((resolve, reject) => {
+function getUsers() {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      if (email === "test@gmail.com" && password === "123456") {
-        resolve("Login successfull!");
-      } else {
-        reject("Inavlid email or password!");
-      }
+      resolve({ id: 1, name: "Abhishek" });
     }, 2000);
   });
 }
 
-loginUser("test@gmail.com", "wrong")
-  .then((message) => {
-    console.log(message);
+function getPosts(userId) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { id: 101, title: "Post 1", userId },
+        { id: 102, title: "Post 2", userId },
+      ]);
+    }, 2000);
+  });
+}
+
+getUsers()
+  .then((user) => {
+    console.log("User : ", user);
+    return getPosts(user.id);
+  })
+  .then((posts) => {
+    console.log("Posts : ", posts);
   })
   .catch((error) => {
     console.log(error);
