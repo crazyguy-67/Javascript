9b3b3a52e5e2a1665bda208dbe360b680d7d5dd2
crazyguy-67 <crazy2hell67@gmail.com>
2026-05-16 18:14:52 +0530

    fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-// callback    = function passed to run later
-// promise     = future success/failure value
-// .then       = handles success
-// .catch      = handles error
-// async       = makes function async
-// await       = waits for promise
-// try/catch   = handles async/await errors
+// Fetch with .then()
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
+
+// FLOW
+//  fetch API
+// → get response
+// → convert response to JSON
+// → use data
+// → catch error if failed
