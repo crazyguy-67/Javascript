eee01551f2af9e4e1aa28b2e8b57394e4e80458c
crazyguy-67 <crazy2hell67@gmail.com>
2026-05-16 18:18:44 +0530

    better version

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-// Fetch with async/await
+// BETTER VERSION
+
 async function getUsers() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch users");
+    }
 
     const data = await response.json();
 
     console.log(data);
   } catch (error) {
-    console.log("Error:", error);
+    console.log("Error:", error.message);
   }
 }
 
 getUsers();
-
-// Wait for API response.
-// Then convert response to JSON.
-// Then print data.
